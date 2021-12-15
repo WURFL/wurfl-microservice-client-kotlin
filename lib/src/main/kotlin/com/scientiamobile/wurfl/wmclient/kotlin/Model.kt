eab8a5c7976ee2549938decc426ade8469da4616
andreacastello <castello.a@gmail.com>
2021-12-15 17:34:32 +0100

    WPC-242: added licenses. Made info data entirely immutable

--- lib/src/main/kotlin/com/scientiamobile/wurfl/wmclient/kotlin/Model.kt
+++ lib/src/main/kotlin/com/scientiamobile/wurfl/wmclient/kotlin/Model.kt
@@ -1,3 +1,15 @@
+/*
+Copyright 2022 ScientiaMobile Inc. http://www.scientiamobile.com
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 package com.scientiamobile.wurfl.wmclient.kotlin
 
 import com.google.gson.annotations.SerializedName
@@ -12,11 +24,17 @@ data class JSONInfoData(
     @SerializedName("wurfl_info")
     val wurflInfo: String,
     @SerializedName("important_headers")
-    var importantHeaders: Array<String>,
+    val importantHeaders: Array<String>,
     @SerializedName("static_caps")
-    var staticCaps: Array<String>,
+    val staticCaps: Array<String>,
     @SerializedName("virtual_caps")
-    var virtualCaps: Array<String>,
+    val virtualCaps: Array<String>,
     @SerializedName("ltime")
-    var ltime: String
+    val ltime: String
 )
+
+/**
+ * WmException is a general purpose exception thrown whenever an unrecoverable error occurs during device detection (ie: no connection available to WM server,
+ * wrong url or port configurations, etc.
+ */
+class WmException(message: String) : Exception(message)
