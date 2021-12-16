743e05c08e4cd5300ad6e2cf40ee56ecb4824319
andreacastello <castello.a@gmail.com>
2021-12-16 12:50:50 +0100

    WPC-242: Added LRUCache unit test using coroutines

--- lib/src/test/kotlin/com/scientiamobile/wurfl/wmclient/kotlin/LRUCacheTest.kt
+++ lib/src/test/kotlin/com/scientiamobile/wurfl/wmclient/kotlin/LRUCacheTest.kt
@@ -1,13 +1,14 @@
 package com.scientiamobile.wurfl.wmclient.kotlin
 
-import kotlin.test.Test
-import kotlin.test.assertEquals
-import kotlin.test.assertNull
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlin.test.*
 
 class LRUCacheTest {
 
     @Test
-    public fun replaceOnMultiAddTest() {
+    fun replaceOnMultiAddTest() {
         val cache = LRUCache<String, Int>(5)
         for (i in 0..5) {
             cache.putEntry(i.toString(), i)
@@ -20,7 +21,7 @@ class LRUCacheTest {
     }
 
     @Test
-    public fun removeOnMaxSizeTest() {
+    fun removeOnMaxSizeTest() {
         val cache = LRUCache<String, Int>(5)
         for (i in 0..5) {
             cache.putEntry(i.toString(), i)
@@ -30,4 +31,46 @@ class LRUCacheTest {
         assertNull(cache.getEntry("0"))
     }
 
+    @Test
+    fun multithreadGetAndClearCacheTest() {
+        val cache = LRUCache<String, Any>(1000)
+        val numOfCoroutines = 32
+        val results = BooleanArray(numOfCoroutines)
+        runBlocking {
+            coroutineScope { // scope for coroutines
+                repeat(numOfCoroutines) { index ->
+                    println("Starting coroutine #: $index")
+                    launch {
+                        // Start task
+                        val userAgentList = TestData.createTestUserAgentList()
+                        try {
+                            var readLines = 0
+                            for (line in userAgentList) {
+                                cache.getEntry(line)
+                                // every 300 detections and only on even threads we clear cache to check it does not cause error
+                                readLines++
+
+                                // every 300 and only on even threads, we clear cache, to see if getEntry handles it without raising errors
+                                if (readLines % 300 == 0 && index % 2 == 0) {
+                                    cache.clear()
+                                } else if (index % 2 != 0) {
+                                    cache.putEntry(line, Any())
+                                }
+                            }
+                            println("Lines read from terminated task #$index : $readLines")
+                            results[index] = true
+
+                        } catch (e: Exception) {
+                            results[index] = false
+                            fail("Test failed due to exception :" + e.message, e)
+                        }
+                    }
+                }
+            }
+        }
+
+        for (i in 0 until numOfCoroutines){
+            assertTrue(results[i])
+        }
+    }
 }
