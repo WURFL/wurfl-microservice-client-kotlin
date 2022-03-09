51d66f6fe582e52da24ad8ed5079500a198fdfde
andreacastello <castello.a@gmail.com>
2022-03-09 17:52:56 +0100

    modified credentials retrieval

--- wmclient/lib/build.gradle.kts
+++ wmclient/lib/build.gradle.kts
@@ -96,8 +96,8 @@ publishing {
                 maven {
                     url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
                     credentials {
-                        username = project.findProperty("mavenUser").toString()
-                        password = project.findProperty("mavenPassword").toString()
+                        username = project.findProperty("mavenCentralUsername").toString()
+                        password = project.findProperty("mavenCentralPassword").toString()
                     }
                 }
             }
