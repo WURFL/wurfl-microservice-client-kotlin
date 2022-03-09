9550b63b1acbb4d70386a39d660aa4bddd21beb4
andreacastello <castello.a@gmail.com>
2022-03-09 19:46:45 +0100

    exp: added explicit classifier

--- wmclient/lib/build.gradle.kts
+++ wmclient/lib/build.gradle.kts
@@ -70,8 +70,12 @@ publishing {
         create<MavenPublication>("wmclient-publish") {
             from(components.findByName("kotlin"))
             artifact("build/libs/wurfl-microservice-kotlin-$version.jar")
-            artifact("build/libs/wurfl-microservice-kotlin-$version-javadoc.jar")
-            artifact("build/libs/wurfl-microservice-kotlin-$version-sources.jar")
+            artifact("build/libs/wurfl-microservice-kotlin-$version-javadoc.jar"){
+                classifier = "javadoc"
+            }
+            artifact("build/libs/wurfl-microservice-kotlin-$version-sources.jar"){
+                classifier = "sources"
+            }
             pom {
                 name.set("WURFL Microservice client for Kotlin")
                 url.set("https://github.com/WURFL/wurfl-microservice-client-kotlin")
